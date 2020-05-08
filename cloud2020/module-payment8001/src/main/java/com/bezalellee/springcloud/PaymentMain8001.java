63f59f6c7dc10a90d296f3eca369d333a74a9d79
bezalellee <bezalellee@gmail.com>
2020-05-08 21:33:02 +0800

    springcloud项目模型0508

---
+++ cloud2020/module-payment8001/src/main/java/com/bezalellee/springcloud/PaymentMain8001.java
@@ -0,0 +1,12 @@
+package com.bezalellee.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableEurekaClient
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class);
+    }
+}
