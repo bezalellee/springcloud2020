fe1eebca6b818a2d7a7f8ec639934231bddcccf6
bezalellee <bezalellee@gmail.com>
2020-05-13 11:17:03 +0800

    springcloud20200513

--- cloud2020/cloud-provider-payment8001/src/main/java/com/bezalellee/springcloud/PaymentMain8001.java
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/bezalellee/springcloud/PaymentMain8001.java
@@ -2,9 +2,10 @@ package com.bezalellee.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class);
