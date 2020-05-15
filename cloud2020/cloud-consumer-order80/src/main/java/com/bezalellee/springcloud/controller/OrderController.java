bb314fddae3735748c3071336795e0f026a14ebd
bezalellee <bezalellee@gmail.com>
2020-05-15 10:07:36 +0800

    springcloud20200515 修改

--- cloud2020/cloud-consumer-order80/src/main/java/com/bezalellee/springcloud/controller/OrderController.java
+++ cloud2020/cloud-consumer-order80/src/main/java/com/bezalellee/springcloud/controller/OrderController.java
@@ -3,7 +3,6 @@ package com.bezalellee.springcloud.controller;
 import com.bezalellee.springcloud.entities.CommonResult;
 import com.bezalellee.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +15,8 @@ import javax.annotation.Resource;
 public class OrderController {
     @Resource
     private RestTemplate restTemplate;
-    @Value("${url.payment_url}")
-    private String PAYMENT_URL;
+
+    private static final String PAYMENT_URL="http://CLOUD-PAYMENT-SERVICE/";
 
     @GetMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment){
